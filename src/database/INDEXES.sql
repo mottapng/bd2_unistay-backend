60fce85dd195f63234f380e8f894142dfa968dac
Victor Motta <victormotta7@gmail.com>
2023-06-19 14:37:06 -0300

    deploy config

--- src/database/INDEXES.sql
+++
@@ -1,5 +0,0 @@
-CREATE INDEX index_users_email ON users (email);
-CREATE INDEX index_listings_user_id ON listings (user_id);
-CREATE INDEX index_images_listing_id ON images (listing_id);
-CREATE INDEX index_message_conversation_id ON message (conversation_id);
-CREATE INDEX index_conversation_renter_id_locator_id ON conversations (renter_id, locator_id);
--- src/database/VIEW.sql
+++
@@ -1,3 +0,0 @@
-CREATE VIEW listings_report(address_id, listing_id, name, description, private_room, price, qnt_bedrooms, qnt_bathrooms, 
-				    qnt_garage, type_id, user_id, created_at, updated_at, street, number, uf, city) AS
-SELECT * FROM listings JOIN address USING (address_id);
